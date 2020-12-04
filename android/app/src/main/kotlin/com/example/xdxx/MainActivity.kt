752014ef99264c923bc7edea9fa81821fc5ba6b6
Chandan kumar <iamlonewolf700@gmail.com>
2020-12-04 13:10:18 +0530

    login ui by chandan

---
+++ android/app/src/main/kotlin/com/example/xdxx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.xdxx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+import 'login/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+// login ui
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          color: Colors.deepOrangeAccent,
+        ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 70,
+            ),
+            const CircleAvatar(
+              radius: 55,
+              backgroundColor: Colors.redAccent,
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/profile.png'),
+                backgroundColor: Colors.white,
+              ),
+            ),
+            const Text(
+              'Login',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                  margin: const EdgeInsets.only(top: 5),
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30))),
+                  child: Form(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 60,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 10),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                hintStyle: TextStyle(color: Colors.black),
+                                prefixIcon: Icon(
+                                  Icons.email,
+                                ),
+                                hintText: 'Username',
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 10),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                hintStyle: TextStyle(color: Colors.black),
+                                prefixIcon: Icon(
+                                  Icons.email,
+                                ),
+                                hintText: 'Email',
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 10),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                hintStyle: TextStyle(color: Colors.black),
+                                prefixIcon: Icon(
+                                  Icons.email,
+                                ),
+                                hintText: 'Password',
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 10),
+                            child: Container(
+                              width: MediaQuery.of(context).size.width,
+                              padding: const EdgeInsets.all(20),
+                              decoration: const BoxDecoration(
+                                  color: Colors.deepOrangeAccent,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10),
+                                  )),
+                              child: const Center(
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const InkWell(
+                            onTap: null,
+                            child: Text(
+                              'Forgot password ?',
+                              style: TextStyle(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 50,
+                          ),
+                          const InkWell(
+                            onTap: null,
+                            child: Text(
+                              "I don't have Account.",
+                              style: TextStyle(
+                                  color: Colors.deepOrangeAccent,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
